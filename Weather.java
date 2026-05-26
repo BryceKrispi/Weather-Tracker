c0228e0631a667cdc250aedc17498eb5587482e2
Bryce Starkman <127551487+BryceKrispi@users.noreply.github.com>
2026-05-26 17:50:39 +0000

    hi

--- Weather.java
+++ Weather.java
@@ -6,14 +6,12 @@ public class Weather {
     double precipitation = 0.0; // Measurement in centimeters (cm)
     String precipitationType = "none"; // If precipitation is 0, default to "none"
     double temp; // Degrees in celsius (°C)
-    boolean safe = false; // Determines if it is safe to leave home
 
     // Constructor
-    public Weather(double precipitation, String precipitationType, double temp, boolean safe){
+    public Weather(double precipitation, String precipitationType, double temp){
         this.precipitation = precipitation;
         this.precipitationType = precipitationType;
         this.temp = temp;
-        this.safe = safe;
     }
 
     //------------------------------------------------
@@ -31,7 +29,25 @@ public class Weather {
         return temp;
     }
 
-    public boolean getSafe(){
-        return safe;
+    // Returns if it is safe to go outside
+    public boolean isSafe(){
+        if (temp <= 0 || temp >= 40 || precipitation >= 10){
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    // toString method
+    public String toString(){
+        if (isSafe() == false){
+            // Not safe
+        return "There are " + precipitation + "cm of " + precipitationType + 
+                ", the temperature is " + temp + " °C." + " Do not go outside!";
+        } else {
+            // Safe
+            return "There are " + precipitation + "cm of " + precipitationType + 
+                ", the temperature is " + temp + " °C." + " It is safe to go outside.";
+        }
     }
 }
