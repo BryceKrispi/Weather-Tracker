3633c5fe7101c429fa967ca683695a86ca64be02
Namish Bansal <145496543+NamishBansal15@users.noreply.github.com>
2026-05-26 22:39:47 -0400

    Namish's changes made from home on 5/26. Added WeatherAPI class, which calls from WeatherAPI and can create Weather object.

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,15 @@ package src;
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) throws Exception {
 
-    public static void main(String[] args){
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a city: ");
+        String city = scanner.nextLine();
+
+        WeatherAPI api = new WeatherAPI();
+        Weather weather = api.getWeather(city);
+
+        System.out.println(weather);
     }
 }
---
+++ src/WeatherAPI.java
@@ -0,0 +1,42 @@
+package src;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import org.json.JSONObject;
+
+public class WeatherAPI {
+    private static final String API_KEY = "your_api_key_here";
+    private static final String BASE_URL = "http://api.weatherapi.com/v1";
+
+    public Weather getWeather(String city) throws Exception {
+
+        // Build and send request
+        String url = BASE_URL + "/current.json?key=" + API_KEY + "&q=" + city.replace(" ", "%20");
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create(url))
+            .GET()
+            .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        // Parse JSON
+        JSONObject json = new JSONObject(response.body());
+        JSONObject current = json.getJSONObject("current");
+
+        // Extract values
+        double temp = current.getDouble("temp_c");
+        double precipitation = current.getDouble("precip_mm") / 10;
+
+        String conditionText = current.getJSONObject("condition").getString("text").toLowerCase();
+        String precipitationType = "none";
+        if (conditionText.contains("rain"))       precipitationType = "rain";
+        else if (conditionText.contains("snow"))  precipitationType = "snow";
+        else if (conditionText.contains("hail"))  precipitationType = "hail";
+        else if (conditionText.contains("sleet")) precipitationType = "sleet";
+
+        // Build and RETURN a Weather object
+        return new Weather(precipitation, precipitationType, temp);
+    }
+}
